774192defa3693129320097c7b0bf6b39f5b1326
Stijn Tintel <stijn@linux-ipv6.be>
2026-02-06 13:48:43 +0200

    netfilter: drop iptables dependency from kmod-nf-ipvs
    These do not exist in the kernel and make it impossible to build
    nftables-only images.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- package/kernel/linux/modules/netfilter.mk
+++ package/kernel/linux/modules/netfilter.mk
@@ -460,7 +460,7 @@ IPVS_MODULES:= \
 define KernelPackage/nf-ipvs
   SUBMENU:=Netfilter Extensions
   TITLE:=IP Virtual Server modules
-  DEPENDS:=@IPV6 +LINUX_6_12:kmod-lib-crc32c +kmod-ipt-conntrack +kmod-nf-conntrack
+  DEPENDS:=@IPV6 +LINUX_6_12:kmod-lib-crc32c +kmod-nf-conntrack +kmod-nf-xtables
   KCONFIG:= \
 	CONFIG_IP_VS \
 	CONFIG_IP_VS_IPV6=y \
@@ -488,7 +488,6 @@ define KernelPackage/nf-ipvs
 	CONFIG_IP_VS_NFCT=y \
 	CONFIG_NETFILTER_XT_MATCH_IPVS
   FILES:=$(foreach mod,$(IPVS_MODULES),$(LINUX_DIR)/net/netfilter/$(mod).ko)
-  $(call AddDepends/ipt,+kmod-ipt-conntrack,+kmod-nf-conntrack)
 endef
 
 define KernelPackage/nf-ipvs/description
