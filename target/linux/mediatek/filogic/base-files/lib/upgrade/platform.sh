aa92cf8e8726db63aafd3422ec5c0c77e7cc18fd
BeeconMini <zhonghaodong@beeconx.com.cn>
2026-04-20 06:37:13 +0000

    适配BeeconMini SEED AC3

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -291,6 +291,7 @@ platform_do_upgrade() {
 		CI_ROOT_UBIPART=ubi
 		nand_do_upgrade "$1"
 		;;
+	beeconmini,seed-ac3|\
 	beeconmini,seed-ac2|\
 	beeconmini,seed-ac1)
 		CI_KERNPART="kernel"
@@ -403,6 +404,7 @@ platform_copy_config() {
 	arcadyan,mozart|\
 	beeconmini,seed-ac1|\
 	beeconmini,seed-ac2|\
+	beeconmini,seed-ac3|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt6000|\
